b7495bb30c5bd906117af5f4cccd986115fa40b5
Vika <kurilova2003@gmail.com>
2023-09-16 17:38:47 +0300

    Updates

--- src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-    baseURL: 'https://api.thecatapi.com/v1/images/',
-    headers: {
-        'x-api-key': 'live_NCRYREPpaHJo9qygk792XOxbJGa04d2Mwer23GbolguztwbIlfoLso409iihFxoG',
-    },
-});
-export default axiosInstance;
